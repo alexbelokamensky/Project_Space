dfa6d2b9d9319c60a904d91b632b4e1417936764
alexbelokamensky <sbelokamenskij4@gmail.com>
2025-12-09 05:19:17 +0200

    updated menu ui

--- interface/screens/main_menu_screen.py
+++ interface/screens/main_menu_screen.py
@@ -3,20 +3,17 @@ import pygame_gui as pgui
 import os
 import sys
 
-from core.config import WINDOW_WIDTH, WINDOW_HEIGHT, IMAGE_PATH, SOUND_PATH
+from core.config import WINDOW_WIDTH, WINDOW_HEIGHT, IMAGE_PATH, SOUND_PATH, THEMES_PATH
 from interface.screens.base_screen import Screen
 
 class MainMenuScreen(Screen):
     def __init__(self, manager):
         self.manager = manager
-        self.ui_manager = pgui.UIManager((WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.start_survival_mode_game = pgui.elements.UIButton(relative_rect=pg.Rect(WINDOW_WIDTH/2-100, WINDOW_HEIGHT/2-140, 200, 80),
+        self.ui_manager = pgui.UIManager((WINDOW_WIDTH, WINDOW_HEIGHT), os.path.join(THEMES_PATH, "menu_theme.json"))
+        self.start_survival_mode_game = pgui.elements.UIButton(relative_rect=pg.Rect(WINDOW_WIDTH/2-100, WINDOW_HEIGHT/2-80, 200, 80),
                                                                text="Survival Mode", manager=self.ui_manager)
-        
-        self.start_parking_mode_game = pgui.elements.UIButton(relative_rect=pg.Rect(WINDOW_WIDTH/2-100, WINDOW_HEIGHT/2-40, 200, 80),
-                                                               text="Parking Mode", manager=self.ui_manager)
 
-        self.exit_game = pgui.elements.UIButton(relative_rect=pg.Rect(WINDOW_WIDTH/2-100, WINDOW_HEIGHT/2+60, 200, 80),
+        self.exit_game = pgui.elements.UIButton(relative_rect=pg.Rect(WINDOW_WIDTH/2-100, WINDOW_HEIGHT/2+30, 200, 80),
                                                                text="Exit", manager=self.ui_manager)
         
         background_image = pg.image.load(os.path.join(IMAGE_PATH, "background/background3.jpg")).convert_alpha()
